f7102b0807fcf81dd959773186874b8b94538152
Dustin Leavins <dustinleavins@gmail.com>
2010-02-12 14:22:09 -0500

    Adding test functionality and test stubs.

--- test/CalculatorD/Test/TestFraction.java
+++ test/CalculatorD/Test/TestFraction.java
@@ -22,6 +22,62 @@ public class TestFraction {
         correctDenominator = BigInteger.valueOf(10);
         assertEquals(f.getNumerator(), correctNumerator);
         assertEquals(f.getDenominator(), correctDenominator);
+
+        // Fraction, Negative, Greater than -1
+        f = new Fraction("-0.1); // -1/10
+        correctNumerator = BigInteger.valueOf(-1);
+        assertEquals(f.getNumerator(), correctNumerator);
+        assertEquals(f.getDenominator(), correctDenominator);
+    }
+
+    @Test
+    public void testIntegerConstructor() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testLongConstructor() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testDoubleValue() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testAddition() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testSubtraction() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testMultiply() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testNegative() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testDivide() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testInverse() {
+        // TODO - Stub
+    }
+
+    @Test
+    public void testEquals() {
+        // TODO - Stub
     }
 
     public static void main (String [] args)
