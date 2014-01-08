3d127f60fea7d52ff819850d2f8304f09eb33af0
Dustin Leavins <dustinleavins@gmail.com>
2014-01-08 14:33:23 -0600

    Calculation test refactoring

--- src/test/java/info/dustin_leavins/calculatord/TestCalculation.java
+++ src/test/java/info/dustin_leavins/calculatord/TestCalculation.java
@@ -2,6 +2,7 @@ package info.dustin_leavins.calculatord;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -12,56 +13,69 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TestCalculation {
 
+    private Calculation calc;
+
+    @Before
+    public void setUp() {
+        calc = new Calculation();
+    }
+
     @Test
-    public void everything() {
-        Calculation calc = new Calculation();
-        Fraction a;
-        Fraction b;
-        Fraction c;
-        Fraction result;
-
-        //---Addition Test---
-        // (1/3) + (5/3) = (6/3) = 2
-        a = new Fraction(1,3);
-        b = new Fraction(5,3);
-        result = new Fraction(2,1);
-        calc.appendNumber(a);
+    public void appendOperationFirst() {
         calc.appendOperation(Operation.PLUS);
-        calc.appendNumber(b);
-        assertTrue(result.equals(calc.getValue()));
+        assertTrue(calc.getValue().equals(Fraction.ZERO));
+    }
 
-        //---Subtraction Test---
+    @Test
+    public void addition() {
+        // (1/3) + (5/3) = (6/3) = 2
+        Fraction result = new Fraction(2,1);
+
+        calc.appendNumber(new Fraction(1,3));
+        calc.appendOperation(Operation.PLUS);
+        calc.appendNumber(new Fraction(5,3));
+        assertTrue(result.equals(calc.getValue()));
+    }
+
+    @Test
+    public void subtraction() {
         // (1/3) - (5/3) = (-4/3)
-        calc.clear();
-        result = new Fraction(-4,3);
-        calc.appendNumber(a);
+        Fraction result = new Fraction(-4,3);
+        
+        calc.appendNumber(new Fraction(1,3));
         calc.appendOperation(Operation.MINUS);
-        calc.appendNumber(b);
+        calc.appendNumber(new Fraction(5,3));
         assertTrue(result.equals(calc.getValue()));
+    }
 
-        //---Multiplication Test---
+    @Test
+    public void multiply() {
         // (1/3) * (5/3) = (5/9)
-        calc.clear();
-        result = new Fraction(5,9);
-        calc.appendNumber(a);
+        Fraction result = new Fraction(5,9);
+
+        calc.appendNumber(new Fraction(1,3));
         calc.appendOperation(Operation.MULTIPLY);
-        calc.appendNumber(b);
+        calc.appendNumber(new Fraction(5,3));
         assertTrue(result.equals(calc.getValue()));
+    }
 
-        //---Division Test---
+    @Test
+    public void divide() {
         // (1/3) / (5/3) = (1/5)
-        calc.clear();
-        result = new Fraction(1, 5);
-        calc.appendNumber(a);
-        calc.appendOperation(Operation.DIVIDE);
-        calc.appendNumber(b);
-        assertTrue(result.equals(calc.getValue()));
+        Fraction result = new Fraction(1, 5);
 
-        //---Mixed Tests---
-        calc.clear();
-        a = new Fraction(5,1);
-        b = new Fraction(4,1);
-        c = new Fraction(99,1);
+        calc.appendNumber(new Fraction(1,3));
+        calc.appendOperation(Operation.DIVIDE);
+        calc.appendNumber(new Fraction(5,3));
+        assertTrue(result.equals(calc.getValue()));
+    }
+
+    @Test
+    public void mixedTests() {
+        Fraction a = new Fraction(5,1);
+        Fraction b = new Fraction(4,1);
+        Fraction c = new Fraction(99,1);
+        Fraction result;
 
         result = new Fraction(100,1);
         calc.appendNumber(a);
